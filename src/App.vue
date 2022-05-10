ae390237b2cd5ba9d67b4e186ac4e73c53710ce9
a653398363 <653398363@qq.com>
2022-05-10 17:28:05 +0800

    Bpmn

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,21 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100vh;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,70 @@
 <script >
-// import { ref } from 'vue'
+import { ref } from 'vue'
 // 引入相关的依赖
 import Modeler from 'bpmn-js/lib/Modeler'
-defineProps({
-  msg: String
-})
+
+import Diagram from './newDiagram'
+
+import 'bpmn-js/dist/assets/diagram-js.css' // 左边工具栏以及编辑节点的样式
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
+
+
+
+import {
+  BpmnPropertiesPanelModule,
+  BpmnPropertiesProviderModule,
+  CamundaPlatformPropertiesProviderModule
+} from 'bpmn-js-properties-panel';
+
+// import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda'
+import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda'
+
+import 'bpmn-js-properties-panel/dist/assets/properties-panel.css' // 右边工具栏样式
+import 'bpmn-js-properties-panel/dist/assets/element-templates.css' // 右边工具栏样式
 
 export default {
-
   data () {
     return {
       bpmnModeler: null
     }
   },
-
   mounted () {
-
+    this.int()
   },
   methods: {
     int () {
       this.bpmnModeler = new Modeler({
-        container: '#canvas'
+        container: '#canvas',
+        propertiesPanel: {
+          parent: '#properties'
+          // parent: '#js-properties-panel'
+        },
+        additionalModules: [
+          // 右边的属性栏
+          BpmnPropertiesPanelModule,
+          BpmnPropertiesProviderModule,
+          CamundaPlatformPropertiesProviderModule
+        ],
+        moddleExtensions: {
+          camunda: camundaModdleDescriptor
+        }
       })
+
+      console.log('bpmn', this.bpmnModeler)
       this.createNewDiagram()
+    },
+
+    async createNewDiagram () {
+      try {
+        await this.bpmnModeler.importXML(Diagram)
+
+        console.log('canvas', this.$refs.canvas)
+        // ...
+      } catch (err) {
+        // err...
+      }
     }
   }
 }
@@ -30,14 +72,33 @@ export default {
 </script>
 
 <template>
-  <div class="">
-    <div id="canvas"></div>
-
+  <div class="modeler">
+    <div id="canvas" class="canvas" ref="canvas"></div>
+    <div id="properties"></div>
   </div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
+.modeler {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+.canvas,
+.modeler {
+  width: 100%;
+  height: 100%;
+}
+
+#properties {
+  position: absolute;
+  top: 20px;
+  right: 16px;
+}
+.panel {
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 300px;
 }
 </style>
