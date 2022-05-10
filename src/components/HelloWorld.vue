ab1f3deaed134caf2dad945ffd6c6e7795f5b454
a653398363 <653398363@qq.com>
2022-05-10 14:04:34 +0800

    vite

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script >
+// import { ref } from 'vue'
+// 引入相关的依赖
+import Modeler from 'bpmn-js/lib/Modeler'
+defineProps({
+  msg: String
+})
+
+export default {
+
+  data () {
+    return {
+      bpmnModeler: null
+    }
+  },
+
+  mounted () {
+
+  },
+  methods: {
+    int () {
+      this.bpmnModeler = new Modeler({
+        container: '#canvas'
+      })
+      this.createNewDiagram()
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="">
+    <div id="canvas"></div>
+
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
