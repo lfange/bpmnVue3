bd73ba66561ae40a20ce2e9b6f1404f65d873236
FanGe <653398363@qq.com>
2022-08-04 14:57:55 +0800

    elementBaseinfo

--- package/panel/base/ElementBaseInfo.vue
+++ package/panel/base/ElementBaseInfo.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
-    defineProps({
-        formState: Object<String | any>
-    })
+
+    // 类型的 props/emit 声明
+    interface FormState {
+      name: String;
+      id: String;
+      tag: String;
+      isExecutable: String;
+      $type: String;
+    }
+
+    const props = defineProps<{ formState: FormState }>()
+
+    function updateS(e: any) {
+      console.log('updateS', props.formState)
+      console.log('updateS', e)
+    }
 </script>
 <!-- <script>
 import { ref, toRefs, defineProps, defineComponent } from "vue";
@@ -10,15 +23,15 @@ export default defineComponent({});
 </script> -->
 <template>
     <a-form-item label="名称" name="username">
-      <a-input v-model:value="formState.username" />
+      <a-input v-model:value="formState.name"  @change="updateS"/>
     </a-form-item>
-    <a-form-item label="ID" name="ID">
-      <a-input v-model:value="formState.ID" />
+    <a-form-item label="ID" name="id">
+      <a-input v-model:value="formState.id" />
     </a-form-item>
-    <a-form-item label="版本标签" name="Tag">
-      <a-input v-model:value="formState.Tag" />
+    <a-form-item label="版本标签" name="tag" v-if="formState.$type === 'bpmn:Process'">
+      <a-input v-model:value="formState.tag" />
     </a-form-item>
-    <a-form-item label="可执行" name="execute">
-      <a-input v-model:value="formState.execute" />
+    <a-form-item label="可执行" name="isExecutable" v-if="formState.$type === 'bpmn:Process'">
+      <a-checkbox v-model:checked="formState.isExecutable"></a-checkbox>
     </a-form-item>
 </template>
