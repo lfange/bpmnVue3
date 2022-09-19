05a80ee13116b536c2cbfadf1e526e3993416652
FanGe <653398363@qq.com>
2022-09-19 16:07:25 +0800

    gitpage

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/ 
 export default defineConfig({
   base: './',
-  plugins: [vue()],
   server: {
     //使用IP能访问
     host: '0.0.0.0',
@@ -23,6 +23,10 @@ export default defineConfig({
       }
     }
   },
+  plugins: [
+    vue(),
+    vueJsx({})
+  ],
   // build: {
   //   assetsDir
   // }
